4644f14f8fa2deedd19d0d00ec773fcd0b0adc9c
Rachael Serur <rachaelse@gmail.com>
2014-02-06 21:33:07 -0500

    Rotations and notes

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-var scrollTotal = 1000;
+var scrollTotal = 10;
 var scrolled = 0; // A variable to keep track of how far we've scrolled.
 var fractionScrolled = scrolled / scrollTotal;
 
 
-// You can read more about the mosuewheel event at https://developer.mozilla.org/en-US/docs/DOM/DOM_event_reference/mousewheel
-if (document.addEventListener) {
+// mousewheel
+// You can read more about the mousewheel event at https://developer.mozilla.org/en-US/docs/DOM/DOM_event_reference/mousewheel
+if (document.addEventListener) 
+{
 	document.addEventListener("mousewheel", MouseWheelHandler, false);
 }
 
-
 var waypoints = document.getElementsByClassName('waypoint');
-for (i = 0; i < waypoints.length; i++) {
+var triangle = document.getElementById('next-triangle');
+
+// clicking
+for (i = 0; i < waypoints.length; i++) 
+{
 	// Here we attach a handler to the click event for every waypoint,
 	// https://developer.mozilla.org/en-US/docs/Web/Reference/Events/click
 	waypoints[i].addEventListener("click", waypointClickHandler, false);
 }
 
-function updateWaypoints() {
+function updateWaypoints() 
+{
 	fractionScrolled = scrolled / scrollTotal;
 
 	// 0 <= fractionScrolled <= 1, so *10 gives us 10; Math.floor rounds down
 	var whichWaypoint = Math.max(0, Math.floor(fractionScrolled * 10) - 1);
 
+	// Iterating through waypoint list in html
 	for (i = 0; i < 10; i++) {
 		// Notice we constructed our li#id names to make this easy
 		var currentWaypoint = document.getElementById('waypoint-' + i);
@@ -39,20 +46,26 @@ function updateWaypoints() {
 	document.getElementById('Countdown').currentTime = fractionScrolled * 38.0;
 }
 
+// This function is called every time there's a click
 function waypointClickHandler(e) {
-	console.log('cilck');
+	console.log('click');
 	for (i = 0; i < waypoints.length; i++) {
 		if (waypoints[i] === this) {
+			// change scroll # based on clicked waypoint
 			scrolled = (i+1)*100;
 			updateWaypoints();
 			console.log(scrolled);
-		}
+		}	
 	}
+	if (triangle === this)
+				scrolled = (i+1)*100;
+				updateWaypoints();
+				console.log(scrolled);		
 }
 
-
+// This function is called every time there's a mousewheelevent
 function MouseWheelHandler(e) {
-	// This function is called every time there's a mousewheelevent
+	
 
 	var rawScrolled = Math.max(-1, Math.min(1, e.wheelDelta));
 	scrolled = Math.min(Math.max(0, scrolled - rawScrolled), scrollTotal);
